754dd92589198f51176d24a423266b632f217f39
Yanyan <72719600+Yanyan07@users.noreply.github.com>
2021-07-02 16:52:14 -0600

    Update Objective4Lab2.java

--- Labs/Objective4Lab2.java
+++ Labs/Objective4Lab2.java
@@ -22,5 +22,6 @@ public class Objective4Lab2{
     System.out.println("The sum of "+num1+" + "+num2+" + "+num3+" = "+(num1+num2+num3));
     System.out.println("The sum of "+dub1+" + "+dub2+" + "+dub3+" = "+(dub1+dub2+dub3));
 
+    keyboard.close();
   }
 }
