add5533dc764da81b94c05f61bb11167723d8999
Yanyan07 <yenj2015@gmail.com>
2021-07-02 14:36:19 -0600

    printing multiple variable

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2{
+
+  public static void main(String[] args){
+    String name = "Yanyan Jiang";
+    int age = 3;
+    String state = "China";
+    System.out.println("Name: "+name+" Age: "+age+" State: "+state);
+  }
+}
