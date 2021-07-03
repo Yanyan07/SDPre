409201dee2e776ebc650ac548a8026ecc3293417
Yanyan <72719600+Yanyan07@users.noreply.github.com>
2021-07-03 11:45:10 -0600

    for loop printing numbers

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,11 @@
+public class Objective8Lab1{
+
+  public static void main(String[] args){
+    for(int i=0; i<10; i+=1){
+      System.out.println(i+1);
+    }
+    for(int i=10; i>0; i-=1){
+      System.out.println(i);
+    }
+  }
+}
